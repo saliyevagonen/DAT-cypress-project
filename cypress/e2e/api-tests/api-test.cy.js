84e569c0cd4639c37d0f2c4b41129990bdcd3b5d
saliyevagonen <143353298+saliyevagonen@users.noreply.github.com>
2024-09-06 10:33:51 -0700

    Update api-test.cy.js

--- cypress/e2e/api-tests/api-test.cy.js
+++ cypress/e2e/api-tests/api-test.cy.js
@@ -14,11 +14,17 @@ describe('DAT API Tests', () => {
 
   it('Scenario 1: API Call and Response Validation', () => {
     // Use the stored response data for validation
+    // The substring(0, 10) is used to limit the date format to YYYY-MM-DD
     const createdAt = responseBody.listBuilder3Service.variants[0].created_at.substring(0, 10);
     const updatedAt = responseBody.listBuilder3Service.variants[0].updated_at.substring(0, 10);
 
-    expect(createdAt).to.match(/^\d{4}-\d{2}-\d{2}$/); // Validate date format
-    expect(updatedAt).to.match(/^\d{4}-\d{2}-\d{2}$/); // Validate date format
+     /*
+    Check if the extracted createdAt and updatedAt strings 
+    match the regular expression /^\d{4}-\d{2}-\d{2}$/, 
+    which represents the YYYY-MM-DD date format.
+    */ 
+    expect(createdAt).to.match(/^\d{4}-\d{2}-\d{2}$/);
+    expect(updatedAt).to.match(/^\d{4}-\d{2}-\d{2}$/);
   });
 
   it('Scenario 2: Display Rules Extraction', () => {
@@ -42,9 +48,11 @@ describe('DAT API Tests', () => {
 
   it('Scenario 3: Time Difference Calculation', () => {
     // Use the stored response data to calculate the time difference
+    // Convert the created_at and updated_at fields from strings into JavaScript Date objects using new Date().
     const createdAt = new Date(responseBody.listBuilder3Service.variants[0].created_at);
     const updatedAt = new Date(responseBody.listBuilder3Service.variants[0].updated_at);
-
+    
+    // Calculate the time difference between the two dates in milliseconds by subtracting createdAt from updatedAt.
     const timeDifference = updatedAt - createdAt;
 
     cy.log(`Time Difference in milliseconds: ${timeDifference}`);
