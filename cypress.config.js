8583338eba24acaea1fa30aeef545941d55ac4ac
saliyevagonen <143353298+saliyevagonen@users.noreply.github.com>
2024-09-06 10:35:39 -0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,9 @@ module.exports = defineConfig({
     supportFile: false,
     viewportWidth: 890,
     viewportHeight: 600,
-    defaultCommandTimeout: 30000,
+    defaultCommandTimeout: 10000, // Timeout for commands (10 seconds)
+    requestTimeout: 15000,        // Timeout for API requests (15 seconds)
+    pageLoadTimeout: 60000,       // Timeout for page loads (1 minute)
     retries: {
       runMode: 0,
       openMode: 0,
